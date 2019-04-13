0a8152f37402061c27db50c09317d7891e9e0de2
hehe145 <tarczynski.r@gmail.com>
2019-04-13 07:25:10 +0200

    1st commit

---
+++ src/main/java/com/hehe145/TheCardGame/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.hehe145.TheCardGame.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
